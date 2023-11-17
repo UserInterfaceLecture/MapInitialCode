15793cce1d92672ca32089f5b420d75880b3eb67
sshl5 <sshl505@naver.com>
2023-11-17 17:55:04 +0900

    recently commit

--- app/src/main/java/com/example/userinterfaceproject/MainActivity.java
+++ app/src/main/java/com/example/userinterfaceproject/MainActivity.java
@@ -7,7 +7,7 @@ import android.os.Bundle;
 import android.view.MenuItem;
 
 import com.example.userinterfaceproject.databinding.ActivityMainBinding;
-
+//11_17
 public class MainActivity extends AppCompatActivity {
 
     @Override
