caecaafb72b1d240d030cd9952077cb196fcbca7
Frightend Chicken <seul7864@naver.com>
2023-12-14 20:52:00 +0900

    category complete

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,14 +42,14 @@ dependencies {
 
 
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.10.0")
+    implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("com.tomergoldst.android:tooltips:1.1.1")
     implementation("com.google.firebase:firebase-storage:20.3.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    implementation(platform("com.google.firebase:firebase-bom:32.6.0"))
+    implementation(platform("com.google.firebase:firebase-bom:32.7.0"))
     // 위의 firebase-bom라이브러리에 맞춰서 버전이 설정됨(firebase-analytics)
     implementation("com.google.firebase:firebase-analytics")
     // 여기까지가 fire base의 기본적인 implementation
